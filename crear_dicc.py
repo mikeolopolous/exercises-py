582e090a4695b459edc4695c04b25178ec9c3644
Miguel Lopez <ing.miguellv@gmail.com>
2023-02-08 14:34:21 -0600

    diccionarios

---
+++ crear_dicc.py
@@ -0,0 +1,9 @@
+cliente = {}
+
+cliente["nombre"] = input("Nombre del cliente: ")
+cliente["apellidos"] = input("Apellidos: ")
+cliente["edad"] = int(input("Edad: "))
+cliente["dni"] = input("DNI: ")
+cliente["total"] = float(input("Total a pagar: $"))
+
+print(cliente)
