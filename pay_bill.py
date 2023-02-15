6094e1431970870386556c596bc99d36993715af
Miguel Lopez <ing.miguellv@gmail.com>
2023-02-15 17:40:04 -0600

    delete files not needed

--- pay_bill.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-
-number_of_persons = int(input("Introduce el número de personas: "))
-str_names = input("Introduce los nombres de las personas separados por comas: ")
-names = str_names.split(", ")
-
-loser = choice(names)
-
-print(f"{loser} es quien debe pagar!")
